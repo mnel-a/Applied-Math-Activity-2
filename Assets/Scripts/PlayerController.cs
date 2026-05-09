f00b7f425d50df6ed432605f7656001a5b9023fe
mnel-a <marinel.anglita@ciit.edu.ph>
2026-05-10 01:44:40 +0800

    Unity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(x, 0, z).normalized;
+
+        transform.Translate(Vector3.forward * Time.deltaTime * 10);
+
+        transform.position += move.normalized * moveSpeed * Time.deltaTime;
     }
 }
